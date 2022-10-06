38fb8a90da0277db5472add828a30fbafd07dde9
NoahGregie <71037472+NoahGregie@users.noreply.github.com>
2022-10-06 09:49:57 -0700

    gun is done, added some enemies

--- Assets/Scripts/Gun/GunController.cs
+++ Assets/Scripts/Gun/GunController.cs
@@ -28,6 +28,18 @@ public class GunController : MonoBehaviour
 
     public float aimSmoothing = 10;
 
+    [Header("Mouse Settings")]
+    public float mouseSensitivity = 1;
+    Vector2 currentRotation;
+    public float weaponsSwayAmount = 10;
+
+    //weapon recoil
+    public bool randomizeRecoil;
+    public Vector2 randomRecoilConstraints;
+    //you only need to assign this if randmoize recoil is off.
+    public Vector2 recoilPattern;
+
+    
 
     private void Start()
     {
@@ -38,7 +50,11 @@ public class GunController : MonoBehaviour
 
     private void Update()
     {
+       
         DetermineAim();
+
+        //if this is on, you cant shoot for somereason
+       //DetermineRotation();
 
 
        if(Input.GetMouseButton(0) && canShoot && currentAmmonInClip > 0)
@@ -46,6 +62,7 @@ public class GunController : MonoBehaviour
             canShoot = false;
             currentAmmonInClip--;
             StartCoroutine(ShootGun());
+            
 
         }
        else if(Input.GetKeyDown(KeyCode.R)&& currentAmmonInClip <clipSize && ammoInReserve>0)
@@ -64,6 +81,20 @@ public class GunController : MonoBehaviour
             }
         }
     }
+    void DetermineRotation()
+    {
+        Vector2 mouseAxis = new Vector2(Input.GetAxisRaw("Mouse x"), Input.GetAxisRaw("Mouse y"));
+
+        mouseAxis *= mouseSensitivity;
+        currentRotation += mouseAxis;
+
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -90, 90);
+
+        transform.localPosition += (Vector3)mouseAxis * weaponsSwayAmount / 1000;
+
+        transform.root.localRotation = Quaternion.AngleAxis(currentRotation.x, Vector3.up);
+        transform.parent.localRotation = Quaternion.AngleAxis(-currentRotation.y, Vector3.right);
+    }
     void DetermineAim()
     {
         Vector3 target = normalLocalPosition;
@@ -74,9 +105,28 @@ public class GunController : MonoBehaviour
         transform.localPosition = desiredPostition;
 
     }
+    
+    void DetermineRecoil()
+    {
+        transform.localPosition -= Vector3.forward * 0.1f;
+
+        if(randomizeRecoil)
+        {
+            float xRecoil = Random.Range(-randomRecoilConstraints.x, randomRecoilConstraints.x);
+            float yRecoil = Random.Range(-randomRecoilConstraints.y, randomRecoilConstraints.y);
+
+            Vector2 recoil = new Vector2(xRecoil, yRecoil);
+
+            currentRotation += recoil;
+        }
+    }
     IEnumerator ShootGun()
     {
+        DetermineRecoil();
         StartCoroutine(MuzzleFlash());
+
+        RayCastForEnemy();
+      
         yield return new WaitForSeconds(fireRate);
         canShoot = true;
     }
@@ -88,5 +138,24 @@ public class GunController : MonoBehaviour
         yield return new WaitForSeconds(0.05f);
         muzzleFlashImage.sprite = null;
         muzzleFlashImage.color = new Color(0,0,0,0);
+    }
+
+    void RayCastForEnemy()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.parent.position, transform.parent.forward, out hit, 1 << LayerMask.NameToLayer("Enemy")))
+                {
+            try {
+                Debug.Log("Hit A Enemy");
+                Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.None;
+                rb.AddForce(transform.parent.transform.forward * 500);
+            
+            }
+            catch { }
+
+        }
+                
+
     }
 }
