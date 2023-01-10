0c4d4b6cc163ea8b36779af3901ac8bb6f083125
BelegarIronhammer <90424729+BelegarIronhammer@users.noreply.github.com>
2023-01-10 09:40:26 -0800

    Added Homing Script for Alternative Scenes
    Added Homing Script for Alternative Scenes

---
+++ Assets/Scripts/EnemyStuf/EnemyHomingScript1.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHomingScript1 : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform target;
+    public float speed;
+    Rigidbody rig;
+    movemen pm;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public Transform player;
+   // public float sightRange, attackRange;
+   // public bool playerInSightRange, playerInAttackRange;
+    public GameObject self;
+    public bool cansee = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        pm = GetComponent<movemen>();
+        target = GameObject.Find("Player").transform;
+        animator = gameObject.GetComponent<Animator>();
+       // animator.SetBool("PlayRun", false);
+
+    }
+
+    // Update is called onc
+    // e per frame
+
+    private void Update()
+    {
+
+       
+    }
+
+
+
+
+    void FixedUpdate()
+
+
+    {
+       
+        
+            Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+            // pos.y = 1f;
+            animator.SetBool("PlayRun", true);
+            transform.LookAt(target);
+
+          rig.MovePosition(pos);
+          //rig.MovePostion(rig.position + transform.forward * speed * Time.deltaTime);
+          //rig.MovePostion(transform.position + target.position * speed * Time.fixedDeltaTime);
+    }
+
+
+   
+}
+
